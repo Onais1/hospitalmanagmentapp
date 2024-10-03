3c44267dd513e79a218ad04323e7ceee451ca878
Onais1 <onaisx@gmail.com>
2024-10-04 00:35:10 +0100

    hospitalApp

---
+++ app/src/main/java/com/example/myapplication/LoginActivity.java
@@ -0,0 +1,63 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class LoginActivity extends AppCompatActivity {
+
+    EditText edUsername, edPassword;
+    Button btn;
+    TextView tv;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+        edUsername = findViewById(R.id.editTextUsername);
+        edPassword = findViewById(R.id.editTextPassword);
+        btn = findViewById(R.id.buttonLoginUser);
+        tv = findViewById(R.id.textViewRegister);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = edUsername.getText().toString();
+                String password = edPassword.getText().toString();
+                if(username.length() == 0 || password.length() == 0) {
+                    Toast.makeText(getApplicationContext(),"Username or Password error, Please fill in again", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getApplicationContext(),"Welcome", Toast.LENGTH_SHORT).show();
+                }
+                Toast.makeText(getApplicationContext(),"Welcome", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+
+        tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this,RegisterActivity.class));
+            }
+        });
+
+    }
+}
